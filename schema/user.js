a338b31d7681c9592ea7fa251d0d7f9440dbb572
김성환 <sunghwan@gimseonghwan-ui-MacBook-Pro.local>
2018-07-10 22:40:28 +0900

    hi

--- schema/user.js
+++ schema/user.js
@@ -14,10 +14,24 @@ var userSchema = new Schema({
 		type : String,
 		required : true
 	},
-	email : String,
-	contact : String
+	admin : Boolean,
+	location : String,
+	meta : {
+		age : Number,
+		email : String,
+		website : String,
+		contact : String
+	},
+	deleted : { type:Boolean, required:true, default:false},
+	create_date : { type: Number, required: true, default : new Date().getTime()},
+	update_date : { type: Number, required: true, default : new Date().getTime()},
 });
 
+userSchema.pre('save', function(next) {
+	this.create_date = new Date().getTime();
+	this.update_date = new Date().getTime();
+	next();
+});
 
 // the schema is useless so far
 // we need to create a model using it
