ad93b3f292ff90e6481a004e45c4afb3ddb009cc
김성환 <sunghwan@gimseonghwan-ui-MacBook-Pro.local>
2018-07-11 16:05:49 +0900

    add post

--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,25 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+module.exports = function(express, user)
+{
+    express.post('api/user', function(req, res){
+        var newUser = new user();
+        newUser.name = req.body.name;
+        newUser.username = req.body.username;
+        newUser.password = req.body.password;
+        newUser.age = req.body.age;
+        newUser.contact = req.body.contact;
+        newUser.email = req.body.email;
+        newUser.save(function(err){
+            if(err){
+                console.error(err);
+                res.json({result: 0});
+                return;
+            }
+            console.log("Create new contact! name: " + contact.name);
+            res.json({result:1})
+        });
+    });
+}
